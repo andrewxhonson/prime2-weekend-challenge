151458a0c4d15f7dd56851cfec2a283ae15f2fb2
Andrew Johnson <ajj@byu.net>
2017-12-27 11:37:53 -0600

    finished Hard Mode

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,11 +3,51 @@ $(document).ready(start);
 function start() {
     console.log('JQ ready');
 
-    $('.operation').on('click', calculate);
+    listenForX();
+    $('.operation').on('click', updateOperation);
+    $('#equals').on('click', calculate);
     $('#clear').on('click', clear);
     getResults();
 
     $('#history').on('click', 'li', recalculate);
+}
+
+function updateOperation() {
+    let operationName = $(this).attr('id');
+    let operation;
+    if (operationName === 'add') {
+        operation = "+";
+    } else if (operationName === 'subtract') {
+        operation = "-";
+    } else if (operationName === 'multiply') {
+        operation = "*";
+    } else {
+        operation = "/";
+    }
+    $('#valueOperation').val(operation);
+    listenForY();
+}
+
+function addValueToX() {
+    let inputValue = $('#valueX').val();
+    let thisButtonValue = $(this).attr('id');
+    $('#valueX').val(inputValue + thisButtonValue);
+}
+function addValueToY() {
+    let inputValue = $('#valueY').val();
+    let thisButtonValue = $(this).attr('id');
+    $('#valueY').val(inputValue + thisButtonValue);
+}
+
+function listenForX() {
+    $('#numbers button').off('click', addValueToX);
+    $('#numbers button').off('click', addValueToY);
+    $('#numbers button').on('click', addValueToX);
+}
+function listenForY() {
+    $('#numbers button').off('click', addValueToX);
+    $('#numbers button').off('click', addValueToY);
+    $('#numbers button').on('click', addValueToY);
 }
 
 function recalculate() {
@@ -16,6 +56,10 @@ function recalculate() {
 }
 
 function sendCalculation(x, y, operation) {
+    listenForX();
+    $('#valueX').val('');
+    $('#valueY').val('');
+    $('#valueOperation').val('');
     $.ajax({
         url: '/calculate',
         method: 'POST',
